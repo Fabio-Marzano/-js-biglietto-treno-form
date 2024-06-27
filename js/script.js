74d88ddc595a5e3f9139c1ef52da5e3552e8a5f0
fabiomarzano <fabiomarzano1993@gmail.com>
2024-06-27 23:00:06 +0200

    [UPD] create-conditions-for-calculate-price

--- js/script.js
+++ js/script.js
@@ -6,7 +6,27 @@ btn.addEventListener('click', function () {
   const age = document.getElementById('age').value;
 
   // CALCOLO DEL PREZZO BASE//
-let price = km *0.21;
+  let price = km * 0.21;
+  let final_price;
+
+  // VERIFICO ETA PASSEGGERO E APPLICO SCONTO A SECONDA DEL VALORE CALCOLATO //
+ if (age == 0) { //MAGGIORE DI 18 ANNI//
+    final_price = price;
+ } 
+
+ else if (age == 1) { //MINORE DI 18 ANNI//
+    final_price = price * 0.8;
+ }
+
+ else if (age == 2) { //OVER 65//
+    final_price = price * 0.6;
+ }
+
+ else { //ALTRE CONDIZIONI//
+    alert('Non hai selezionato eta. Verra applicato il costo base');
+ }
+  
+ 
 
 
 })  
