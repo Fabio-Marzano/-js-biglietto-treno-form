ac728afc7b806157d310b99965a19d9be5ce6b0f
fabiomarzano <fabiomarzano1993@gmail.com>
2024-06-27 18:03:37 +0200

    [UPD] insert-input-button-in-js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// RECUPERARE PULSANTE DAL DOM//
+const btn = document.getElementById('calculate');
+
+btn.addEventListener('click', function () {
+  const km = document.getElementById('km').value;
+  console.log(km);
+})  
