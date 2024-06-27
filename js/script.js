51fce9d3ef90dc3dbc729e389f280c2535617f36
fabiomarzano <fabiomarzano1993@gmail.com>
2024-06-27 23:47:40 +0200

    [UPD] active-function-on-reset-button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // RECUPERARE PULSANTE DAL DOM//
 const btn = document.getElementById('calculate');
+const btn_reset = document.getElementById('reset');
 // IL PULSANTE RESTA IN ATTESA DI UN CLICK//
 btn.addEventListener('click', function () {
   const km = document.getElementById('km').value;
@@ -25,12 +26,21 @@ btn.addEventListener('click', function () {
       alert('Non hai selezionato eta. Verra applicato il costo base');
     }
 
-    
+    //GENERO IL MESSAGGIO E LO INSERISCO NEL DOM//
     document.getElementById('final_price').innerHTML = `Il tuo biglietto costa ${final_price.toFixed(2)}€`
     
  
 
 
 })  
+
+btn_reset.addEventListener('click', function () {
+    //SVUOTO I CAMPI INPUT//
+    document.getElementById('km').value = '';
+    document.getElementById('age').value = '';
+
+    //SVUOTO IL BIGLIETTO//
+    document.getElementById('final_price').innerHTML = '';
+})
 
 
