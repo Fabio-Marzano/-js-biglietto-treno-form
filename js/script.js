52c06b21b18f46acebedbf7c5e49570adf9f8e5e
fabiomarzano <fabiomarzano1993@gmail.com>
2024-06-27 23:29:57 +0200

    [UPD] insert-final-price-in-html

--- js/script.js
+++ js/script.js
@@ -10,24 +10,27 @@ btn.addEventListener('click', function () {
   let final_price;
 
   // VERIFICO ETA PASSEGGERO E APPLICO SCONTO A SECONDA DEL VALORE CALCOLATO //
- if (age == 0) { //MAGGIORE DI 18 ANNI//
-    final_price = price;
- } 
+    if (age == 0) { //MAGGIORE DI 18 ANNI//
+      final_price = price;
+    } 
+    else if (age == 1) { //MINORE DI 18 ANNI//
+      final_price = price * 0.8;
+    }
 
- else if (age == 1) { //MINORE DI 18 ANNI//
-    final_price = price * 0.8;
- }
+    else if (age == 2) { //OVER 65//
+      final_price = price * 0.6;
+    }
 
- else if (age == 2) { //OVER 65//
-    final_price = price * 0.6;
- }
+    else { //ALTRE CONDIZIONI//
+      alert('Non hai selezionato eta. Verra applicato il costo base');
+    }
 
- else { //ALTRE CONDIZIONI//
-    alert('Non hai selezionato eta. Verra applicato il costo base');
- }
-  
+    
+    document.getElementById('final_price').innerHTML = `Il tuo biglietto costa ${final_price.toFixed(2)}€`
+    
  
 
 
 })  
+
 
